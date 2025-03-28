1fe81bdd203318376b45b814f5d0844eb4b6e773
Kara Moscoe <kmoscoe@google.com>
2025-03-28 09:31:50 -0700

    Working on hybrid code

--- run_local_docker.sh
+++ run_local_docker.sh
@@ -145,103 +145,9 @@ GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
 NC="\033[0m" # No Color
 
-# Run data container in hybrid mode
-run_data_hybrid() {
-  check_app_credentials
-  if [ "$RELEASE" == "latest" ]; then
-    docker pull gcr.io/datcom-ci/datacommons-data:latest
-  fi
-  schema_update='""'
-  schema_update_text=""
-  if [ "$SCHEMA_UPDATE" == true ]; then
-    schema_update="-e DATA_UPDATE_MODE=schemaupdate"
-    schema_update_text=" in schema update mode"
-  fi
-  echo -e "${GREEN}Starting Docker data container with '$RELEASE' release${schema_update_text} and writing output to Google Cloud...${NC}\n"
-  docker run -it \
-  --env-file $ENV_FILE \
-  ${schema_update//\"/} \
-  -e GOOGLE_APPLICATION_CREDENTIALS=/gcp/creds.json \
-  -v $HOME/.config/gcloud/application_default_credentials.json:/gcp/creds.json:ro \
-  -v $INPUT_DIR:$INPUT_DIR \
-  gcr.io/datcom-ci/datacommons-data:${RELEASE}
-}
-
-help() {
-cat << EOF
-Usage:
-
-./run_local_docker.sh [--env_file|-e <env.list file path>] [--mode|m run ] 
-  container|-c service|data [--release|-r latest|stable] [--image|-i <custom image name:tag>] 
-  [--schema_update|s] 
-
-./run_local_docker.sh [--env_file|-e <env.list file path>] [--mode|m run ] 
-  [container|-c data] [--release|-r latest|stable] --schema_update|s 
-
-./run_local_docker.sh [--env_file|-e <env.list file path>] --mode|m build_run 
-  --image <custom image name:tag> [container|-c service]
-
-Options:
-
---env_file|-e <path to env.list file> 
-  Optional: The path and file name of the environment file env.list. 
-  Default: custom_dc/env.list
-  Use this option to maintain multiple alternate environment files with
-  different settings and directories (helpful for testing).
-      
---mode|m
-  Optional: The different Docker commands to run. 
-  Default: run: Runs data or service container.
-  Other options are:
-  * build_run: Build a custom image and run service container.
-
---container|-c service|data
-  Required in 'run' mode.
-  Optional in 'build_and_run' mode; only option is 'service'.
-  * service: Only run the service container. Use this to run a local service reading data from Cloud SQL.
-  * data: Only run the data container. Use this to run a local data container writing output to Cloud SQL.
-
---release|r stable|latest
-  Optional: run the prebuilt 'stable' image provided by Data Commons team.
-  Other options:
-  * latest: Run the 'latest' release provided by Data Commons team. 
-  Only valid in 'run' mode; ignored in 'build_and_run'.
-
---image <custom image name:tag>
-  Optional in 'run' mode: the name and tag of the previously built custom image to run in the service container.
-  Required in 'build_and_run' mode: the name and tag of the custom image to build and run.
-  Ignored with 'data' container.
-     
---schema_update|-s
-  Optional. In the rare case that you get a 'SQL checked failed' error in
-  your running service, you can set this to run the data container in 
-  schema update mode, which skips embeddings generation and completes much faster.
-  Only valid with 'data' container and 'run' mode. Ignored otherwise.
-
-Examples:
-
-./run_hybrid_docker.sh --container service --release latest
-  Start the service container, using the prebuilt latest release.
-
-./run_hybrid_docker.sh --mode build_run --image my-datacommons:dev
-  Build a custom image and start the service container.
-
-./run_hybrid_docker.sh --container data
-  Run the data container.
-
-For more details, see https://docs.datacommons.org/custom_dc/advanced.html
-EOF
-}
-
 cd $(dirname "$0")
 
 set -e
-
-# Define color codes
-RED="\033[1;31m"
-GREEN="\033[1;32m"
-YELLOW="\033[1;33m"
-NC="\033[0m" # No Color
 
 # Functions for running Docker commands
 ##############################################
@@ -403,7 +309,7 @@ while true; do
       if [ -f "$2" ]; then
         ENV_FILE="$2"
       else
-        echo -e "${RED}Error:${NC} File does not exist.\nPlease specify a valid path and file name." 1>&2
+        echo -e "${RED}ERROR:${NC} File does not exist.\nPlease specify a valid path and file name." 1>&2
         exit 1
       fi
       shift 2
@@ -413,7 +319,7 @@ while true; do
         ACTIONS="$2"
         shift 2  
       else
-        echo -e "${RED}Error:${NC} That is not a valid action. Valid options are:\nrun\nbuild\nbuild_run\nbuild_upload\nupload\n" 1>&2
+        echo -e "${RED}ERROR:${NC} That is not a valid action. Valid options are:\nrun\nbuild\nbuild_run\nbuild_upload\nupload\n" 1>&2
         exit 1
       fi
       ;;
@@ -422,7 +328,7 @@ while true; do
         CONTAINER="$2"
         shift 2
       else
-        echo -e "${RED}Error:${NC} That is not a valid container option. Valid options are:\nall\nservice\n" 1>&2
+        echo -e "${RED}ERROR:${NC} That is not a valid container option. Valid options are:\nall\nservice\n" 1>&2
         exit 1
       fi
       ;;
@@ -431,13 +337,13 @@ while true; do
         RELEASE="$2"
         shift 2
       else
-        echo -e "${RED}Error:${NC} That is not a valid release option. Valid options are:\nstable\nlatest\n" 1>&2
+        echo -e "${RED}ERROR:${NC} That is not a valid release option. Valid options are:\nstable\nlatest\n" 1>&2
         exit 1
       fi
       ;;
     -i | --image)
       if [ "$2" == "latest" ] || [ "$2" == "stable" ]; then
-        echo -e "${RED}Error:${NC} That is not a valid custom image name. Did you mean to use the '--release' option?\n" 1>&2
+        echo -e "${RED}ERROR:${NC} That is not a valid custom image name. Did you mean to use the '--release' option?\n" 1>&2
         exit 1
       else
        IMAGE="$2"
@@ -466,21 +372,23 @@ done
 
 # Handle garbage input (getopt doesn't do it)
 if [ $# -gt 0 ]; then
-  echo -e "${RED}Error: ${NC} Invalid input.\nPlease try again. See '--help' for correct usage.\n" 1>&2
+  echo -e "${RED}ERROR: ${NC} Invalid input.\nPlease try again. See '--help' for correct usage.\n" 1>&2
   exit 1
 fi
 
 # Get options from the selected env.list file
 source "$ENV_FILE"
 
+# Set defaults for hybrid mode
+----------------------------------------------------
 # Determine hybrid mode and set a variable to true for use throughout the script
 if [[ $INPUT_DIR == *"gs://"* ]] && [[ $OUTPUT_DIR == *"gs://"* ]]; then
   service_hybrid=true
 elif [[ $INPUT_DIR != *"gs://"* ]] && [[ $OUTPUT_DIR == *"gs://"* ]]; then
   data_hybrid=true
 elif [[ $INPUT_DIR == *"gs://"* ]] && [[ $OUTPUT_DIR != *"gs://"* ]]; then
-  echo -e "${RED}Error:$NC 
-  data_hybrid=true
+  echo -e "${RED}ERROR:$NC Invalid data directory settings in env.list file. Please set your OUTPUT_DIR to a Cloud Path or your INPUT_DIR to a local path.\n" 1>&2
+  exit 1
 fi
 
 # Handle various error conditions
@@ -492,13 +400,13 @@ fi
 #-------------------------------------------------------------
 # Needed for docker run -v option
 if [ -z "$INPUT_DIR" ] || [ -z "$OUTPUT_DIR" ]; then
-  echo -e "${RED}Error:${NC} Missing input or output data directories.\nPlease set 'INPUT_DIR' and 'OUTPUT_DIR' in your env.list file.\n" 1>&2
+  echo -e "${RED}ERROR:${NC} Missing input or output data directories.\nPlease set 'INPUT_DIR' and 'OUTPUT_DIR' in your env.list file.\n" 1>&2
   exit 1
 fi
 
 # Needed for docker tag and push
 if  [[ ( "$ACTIONS" == *"upload"* ) && ( -z "$GOOGLE_CLOUD_PROJECT" || -z "$GOOGLE_CLOUD_REGION" )]]; then
-  echo -e "${RED}Error:${NC} Missing GCP project and region settings.\nPlease set 'GOOGLE_CLOUD_PROJECT' and/or 'GOOGLE_CLOUD_REGION' in your env.list file.\n" 1>&2
+  echo -e "${RED}ERROR:${NC} Missing GCP project and region settings.\nPlease set 'GOOGLE_CLOUD_PROJECT' and/or 'GOOGLE_CLOUD_REGION' in your env.list file.\n" 1>&2
   exit 1
 fi
 
@@ -506,13 +414,13 @@ fi
 #-------------------------------------------------------------
 # Missing required custom image for build and upload
 if [ "$ACTIONS" != "run" ] && [ -z "$IMAGE" ]; then
-  echo -e "${RED}Error:${NC} Missing an image name and tag for build and/or upload.\nPlease use the -'-image' or '-i' option with the name and tag of the custom image you are building or have already built.\n" 1>&2
+  echo -e "${RED}ERROR:${NC} Missing an image name and tag for build and/or upload.\nPlease use the -'-image' or '-i' option with the name and tag of the custom image you are building or have already built.\n" 1>&2
   exit 1
 fi
 
 # Missing package for upload; not an error, just info
 if [[ "$ACTIONS" == *"upload"* ]] && [ -z $PACKAGE ]; then
-  echo -e "${YELLOW}Info:${NC} No '--package' option specified."
+  echo -e "${YELLOW}INFO:${NC} No '--package' option specified."
   echo -e "The target image will use the same name and tag as the source image '$IMAGE'.\n"
   sleep 3
   # Assign image name
@@ -522,10 +430,9 @@ fi
 # Handle invalid option combinations and reset to valid (others are silently 
 # ignored and handled by the case statement)
 #--------------------------------------------------------------------
-if [ "$SCHEMA_UPDATE" == true ] && [ "$CONTAINER" == "service"];then
-  CONTAINER="data"
-fi
-
+if [ "$SCHEMA_UPDATE" == true ] && [ "$CONTAINER" == "service" ];then
+  CONTAINER="all"
+fi  
 
 # Call Docker commands
 ######################################
